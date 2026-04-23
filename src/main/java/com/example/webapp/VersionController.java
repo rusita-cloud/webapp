077ceae4be20d3c9f0fca91ac00e9641bb48abb9
Cloud <rusita.cloud@gmail.com>
2026-04-23 16:38:35 +0900

    [Version 1.0.0] Jenkins + ArgoCD Demo Webapp

--- src/main/java/com/example/webapp/VersionController.java
+++ src/main/java/com/example/webapp/VersionController.java
@@ -14,7 +14,7 @@ public class VersionController {
     public String getDashboard(Model model) {
         // CI/CD 테스트 시 아래 두 변수의 값을 변경하며 테스트합니다.
         String version = "v1.0.0";
-        String bgColor = "##3498db"; // 초기 색상: 파란색
+        String bgColor = "##3498db"; // 초기 색상: 파란색 
 
         // 캐시가 아닌 새로 배포된 서버임을 확인하기 위한 서버 시간
         String serverTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
